f4eeb8e08cc621c8012cadd6b2675a4bbd60eee7
unknown <you@example.com>
2025-07-10 11:02:13 +0700

    初始化项目 Q4.5

---
+++ CMU_SE_652115520/S3T1/se331/lab2_2/331-intro-to-vite/src/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import {RouterLink,RouterView} from 'vue-router'
+</script>
+
+<template>
+  <div class="wrapper">
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <div>
+  <header>
+    <div class="wrapper">
+      <h1>Vite + Vue</h1>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+    <RouterView />
+  </div>
+</template>
+
+
+
+<style scoped>
+  #layout {
+    font-family: Arial, Helvetica, sans-serif;
+    -webkit-font-smothing:antialiased; 
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  nav {
+    padding: 30px;
+  }
+
+  nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+
+  nav a.router-link-active {
+    color: #42b983;
+  }
+</style>

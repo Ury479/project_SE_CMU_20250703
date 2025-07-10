b4519ac7d7808bf438039f2384d823962ea243f9
unknown <you@example.com>
2025-07-10 12:15:41 +0700

    界面清空，正常显示Home 界面
    去掉@，改为相对路径

--- CMU_SE_652115520/S3T1/se331/lab2_2/331-intro-to-vite/src/App.vue
+++ CMU_SE_652115520/S3T1/se331/lab2_2/331-intro-to-vite/src/App.vue
@@ -3,24 +3,16 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="wrapper">
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <div>
+  <div id="layout">
     <header>
       <div class="wrapper">
-        <h1>Vite + Vue</h1>
         <nav>
           <RouterLink to="/">Home</RouterLink>
           <RouterLink to="/about">About</RouterLink>
         </nav>
       </div>
     </header>
+
     <RouterView />
   </div>
 </template>
--- CMU_SE_652115520/S3T1/se331/lab2_2/331-intro-to-vite/src/view/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>Welcome to the Home Page</h1>
-  </div>
-</template>
-
-<script setup lang="ts">
-// 你可以在这里添加逻辑
-</script>
